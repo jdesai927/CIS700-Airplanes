4bc9619a6fa9bee29c0c5f9bc4cd03d4a64e022b
Tanveer Gill <tanveergill89@gmail.com>
2013-11-12 15:28:38 -0500

    zonewise locking. simulation: initial code

--- PlaneState.java
+++ PlaneState.java
@@ -7,6 +7,21 @@ public class PlaneState
   {
     plane = p;
   }
+
+  // copy constructor
+  public PlaneState(PlaneState planeState)
+  {
+      this(planeState.plane);
+      if (planeState.path != null)
+        this.path = new Trajectory(planeState.path);
+      this.state = planeState.state;
+      this.bearingOrig = planeState.bearingOrig;
+      this.landingLock = planeState.landingLock;
+      if (planeState.currentZone != null)
+        this.currentZone = new Zone(planeState.currentZone);
+      if (planeState.simulationTrajectory != null)
+        this.simulationTrajectory = new Trajectory(planeState.simulationTrajectory);
+  }
   
   Plane plane;
   Trajectory path;
@@ -21,6 +36,8 @@ public class PlaneState
 
   double bearingOrig = 0;
   boolean landingLock = false;
+  Zone currentZone;
+  Trajectory simulationTrajectory;
  
   public void setPath(Trajectory t) {
 	  path = t;
--- Trajectory.java
+++ Trajectory.java
@@ -6,9 +6,23 @@ public class Trajectory {
 	
 	private Shape path;
 	
+  // states
+  public enum TrajectoryType
+  {
+    LINE, ORBIT;
+  }
+  TrajectoryType trajectoryType;
+
 	public Trajectory(Shape s) {
 		path = s;
 	}
+
+  // copy constructor
+  public Trajectory(Trajectory trajectory)
+  {
+    this(trajectory.getPath());
+    this.trajectoryType = trajectory.trajectoryType;
+  }
 	
 	public Shape getPath() {
 		return path;
